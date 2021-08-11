e6824dff5c555af21bc8941db1137b92f4fa7f77
Emerson Galdino da Costa <emerson.costa-terceiro@cea.com.br>
2021-08-11 12:11:01 -0300

    inicio do projeto de teste da plataforma

---
+++ cypress/integration/Portal/index.spec.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+
+describe('Open page initial project', () => {
+    it('Should open success', () => {
+        cy.visit('/');
+        cy.title().should('eq', 'Ponto Eletrônico');
+    });
+
+})
+
+describe('Open page create new user', () => {
+    it('Should open check is page create user is open', () => {
+        cy.get('a').click()
+    });
+
+    it('Should page create user is open, back to home', () =>{
+        cy.get('a').click()
+        cy.title().should('eq', 'Ponto Eletrônico');
+
+    })
+})
+
+
+describe('Login in platarfom', () => {
+    it('Should page open include values for login', () =>{
+        cy.get(':nth-child(1) > .ui > input').type('Galdino');
+        cy.get(':nth-child(2) > .ui > input').type('123456');
+
+        cy.get(':nth-child(1) > .ui > input').should('have.value','Galdino');
+        cy.get(':nth-child(2) > .ui > input').should('have.value','123456');
+        cy.get('.segment > .blue').click()
+
+    })
+})
